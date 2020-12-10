e445ab01fc7e947478373ff1ad69ebfd3bc78522
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2020-12-09 22:53:50 -0500

    Add follow up attempts for answering questions

--- qa_web_app.py
+++ qa_web_app.py
@@ -9,7 +9,7 @@ by a POST request to '/ask'.
 The app sends back an answer as a string.
 """
 import os
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 from sentence_transformers import SentenceTransformer
 from wit import Wit
@@ -30,6 +30,16 @@ REFERENCE_TEXT_PATH = "data/cleaned_jurafsky_and_martin.txt"
 class Agent:
     QUESTION_INTENT = "question"
     EXIT_INTENT = "exit"
+    YES_INTENT = "yes"
+    NO_INTENT = "no"
+    YES_NO_INTENTS = {YES_INTENT, NO_INTENT}
+    # Discourse States:
+    # Track whether we're in special states in conversation.
+    # QA follow up, after we've just answered a question, seeing if should try another.
+    QA_FOLLOW_UP = "QA-ing"
+    INTRO = "intro"
+    NEUTRAL = "neutral"
+
 
     def __init__(self, atam_client_access_token, debug=True):
         # Set debug mode to indicate whether to log full response from wit.ai
@@ -38,6 +48,10 @@ class Agent:
         self._last_q = ""
         self._qud = ""
         self._wit = Wit(atam_client_access_token)
+        # track whatever state the agent is currently in
+        self.current_state = self.INTRO
+        self.responses = []
+
         # Provides a dictionary from intentions to hardcoded responses.
         with open("responses.json", encoding="utf8") as fp:
             self._hardcoded_responses = json.load(fp)
@@ -120,13 +134,9 @@ class Agent:
             # TODO handle quitting behavior
             pass
         elif intent_name == self.QUESTION_INTENT:
-            # Student asked a question. Do a search for a relevant answer.
-            entities: Optional[Dict] = response.get("entities", None)
-            response = self.lookup_reference_answer(entities)
-            if response:
-                return f"I found this in the course materials: \"\n {response}\n\n\" Is that helpful?"
-            # if we couldn't return a chunk from the reference material, bail
-            return "Sorry, I couldn't seem to find a good answer for your question."
+            return self.first_question_response_attempt(response)
+        elif intent_name in self.YES_NO_INTENTS and self.current_state == self.QA_FOLLOW_UP:
+            return self.qa_follow_up(intent_name)
 
         """
         ...TODO return whatever other message we think is right,
@@ -136,7 +146,32 @@ class Agent:
 
         return "Yes. " + question
 
-    def lookup_reference_answer(self, entities: Optional[Dict]) -> Optional[str]:
+    def first_question_response_attempt(self, response):
+        """Student asked a question. Do a search for a relevant answer."""
+        entities: Optional[Dict] = response.get("entities", None)
+        self.responses.extend(self.lookup_reference_answer(entities))
+        if self.responses:
+            self.current_state = self.QA_FOLLOW_UP
+            return f"I found this in the course materials: \"\n {self.responses.pop(0)}\n\n\" Is that helpful?"
+        # if we couldn't return a chunk from the reference material, bail
+        return "Sorry, I couldn't seem to find a good answer for your question."
+
+    def qa_follow_up(self, intent_name):
+        """
+        When in QA follow up state, tries to pop potential responses.
+        """
+        if intent_name == self.NO_INTENT and self.responses:
+            return f"I found this in the course materials: \"\n {self.responses.pop(0)}\n\n\" Is that helpful?"
+        elif intent_name == self.NO_INTENT and not self.responses:
+            self.current_state = self.NEUTRAL
+            return "Sorry I couldn't answer that. I'm still learning. What else can I do for you?"
+        else:
+            # Clear responses, since question answered
+            self.responses = []
+            self.current_state = self.NEUTRAL
+            return "great, glad it helped!"
+
+    def lookup_reference_answer(self, entities: Optional[Dict]) -> Optional[List[str]]:
         """
         Grab the search_query entities identified by WIT for a question intent, and do
         a lookup for chunks from the text.
@@ -147,8 +182,8 @@ class Agent:
             # Not the best way to grab all the search terms, but maybe good enough
             search_query = " ".join([d["value"] for d in search_queries])
             if search_query:
-                # Assume best chunk is the one to use for now
-                return self.search.query_top_chunks(search_query, window_size=3)[0]
+                # Use 3 tries to get useful info for now
+                return self.search.query_top_chunks(search_query, window_size=3, chunks=3)
         return None
 
 
